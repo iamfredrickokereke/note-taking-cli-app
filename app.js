41fefec221ca0864aab2e8190c08015400de0561
Kelly online <kellyonline2016@gmail.com>
2020-05-19 22:58:46 +0100

    pull args from process

---
+++ app.js
@@ -0,0 +1,13 @@
+const command = process.argv[2];
+
+// check data argument passed
+
+if (command === 'add') {
+    console.log('Adding note...');
+    
+}else if (command === 'remove') {
+    console.log('Removing note...');
+    
+}
+
+
