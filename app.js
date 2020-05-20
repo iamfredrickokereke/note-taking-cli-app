6503123e01c265503d634c59a406348858a6bd34
Kelly online <kellyonline2016@gmail.com>
2020-05-20 08:28:20 +0100

    ADDED yargs command button

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ const command = process.argv[2];
 
 console.log(yargs.argv);
  
-
+yargs.command({
+    command : 'Add',
+    description : "Adds a new note",
+    handler : () =>{
+        console.log('it works!');
+        
+    }
+})
 
 // check data argument passed
 
-if (command === 'add') {
-    console.log('Adding note...');
-    console.log(process.argv);
+// if (command === 'add') {
+//     console.log('Adding note...');
+//     console.log(process.argv);
     
     
-}else if (command === 'remove') {
-    console.log('Removing note...');
+// }else if (command === 'remove') {
+//     console.log('Removing note...');
     
 }
 
