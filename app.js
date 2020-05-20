aa50e95af6a5497469447ebe69823c1dcfcdc87a
Kelly online <kellyonline2016@gmail.com>
2020-05-20 13:10:36 +0100

    added more features

--- app.js
+++ app.js
@@ -6,11 +6,39 @@ const yargs = require('yargs');
 yargs.command({
     command: 'Add',
     description: 'Adds a new note',
-    handler: () =>{
-        console.log('it works!');
+    handler: () => {
+        console.log('Adding a note...!');
         
     }
 })
+
+yargs.command({
+    command: 'Remove',
+    description: 'Removes a note',
+    handler: () => {
+        console.log('Removing a note...');
+        
+    }
+})
+
+yargs.command({
+    command: 'List',
+    description: 'Shows all notes',
+    handler: () => {
+        console.log('Showing all notes...');
+        
+    }
+})
+
+yargs.command({
+    command: 'Read',
+    description: 'Read a note',
+    handler: () => {
+        console.log('Reading notes...');
+        
+    }
+})
+
 yargs.version('1.0.0')
 
 console.log(yargs.argv);
